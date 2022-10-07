b7e770a350fa680840ac1a7d24013608a614f03f
hooray-sandpang <kemutakejosui@gmail.com>
2022-10-07 17:45:28 +0900

    initial commit by running program for the first time

--- alphapose/version.py
+++ alphapose/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Thu Jul 21 17:10:51 2022
+# TIME: Wed Oct  5 18:22:33 2022
 
-__version__ = '0.6.0+29ace8c'
-short_version = '0.6.0'
+__version__ = '0.5.0+a94dae6'
+short_version = '0.5.0'
